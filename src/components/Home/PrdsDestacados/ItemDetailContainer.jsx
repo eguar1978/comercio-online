53b5a0a89decf0ec14ee2fc52689154847691369
eguar1978 <maileguar@gmail.com>
2020-12-26 16:48:01 -0300

    Antes de trabajar con rutas

--- src/components/Home/PrdsDestacados/ItemDetailContainer.jsx
+++ src/components/Home/PrdsDestacados/ItemDetailContainer.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {useEffect, useState} from 'react';
 import { SiFacebook, SiTwitter, SiWhatsapp } from "react-icons/si";
+import { FaRegStar, FaStar } from "react-icons/fa";
 import Loading from '../../varios/Loading';
 import ItemCount from './ItemCount'
 
@@ -37,51 +38,41 @@ const ItemDetailContainer = () => {
             {
                 product ?
             <>
-                <section class="text-gray-700 body-font overflow-hidden bg-white">
-                    <div class="container px-5 py-24 mx-auto">
-                    <div class="lg:w-4/5 mx-auto flex flex-wrap">
-                        <img alt="ecommerce" class="lg:w-1/2 w-full object-cover object-center rounded border border-gray-200" src={product.foto} />
-                        <div class="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
-                        <h2 class="text-sm title-font text-gray-500 tracking-widest">{product.marca}</h2>
-                        <h1 class="text-gray-900 text-3xl title-font font-medium mb-1">{product.nombre}</h1>
-                        <div class="flex mb-4">
-                            <span class="flex items-center">
-                            <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 text-red-500" viewBox="0 0 24 24">
-                                <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
-                            </svg>
-                            <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 text-red-500" viewBox="0 0 24 24">
-                                <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
-                            </svg>
-                            <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 text-red-500" viewBox="0 0 24 24">
-                                <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
-                            </svg>
-                            <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 text-red-500" viewBox="0 0 24 24">
-                                <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
-                            </svg>
-                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 text-red-500" viewBox="0 0 24 24">
-                                <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
-                            </svg>
-                            <span class="text-gray-600 ml-3">4 Reseñas</span>
+                <section className="text-gray-700 body-font overflow-hidden bg-white">
+                    <div className="container px-5 py-24 mx-auto">
+                    <div className="lg:w-4/5 mx-auto flex flex-wrap">
+                        <img alt="ecommerce" className="lg:w-1/2 w-full object-cover object-center rounded border border-gray-200" src={product.foto} />
+                        <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
+                        <h2 className="text-sm title-font text-gray-500 tracking-widest">{product.marca}</h2>
+                        <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">{product.nombre}</h1>
+                        <div className="flex mb-4">
+                            <span className="flex items-center">
+                                <FaStar className="w-4 h-4 text-red-500"/>
+                                <FaStar className="w-4 h-4 text-red-500"/>
+                                <FaStar className="w-4 h-4 text-red-500"/>
+                                <FaStar className="w-4 h-4 text-red-500"/>
+                                <FaRegStar className="w-4 h-4 text-red-500" />
+                            <span className="text-gray-600 ml-3">4 Reseñas</span>
                             </span>
-                            <span class="flex ml-3 pl-3 py-2 border-l-2 border-gray-200">
-                            <a class="text-gray-500">
+                            <span className="flex ml-3 pl-3 py-2 border-l-2 border-gray-200">
+                            <a className="text-gray-500">
                                 <SiFacebook />
                             </a>
-                            <a class="ml-2 text-gray-500">
+                            <a className="ml-2 text-gray-500">
                                 <SiTwitter />
                             </a>
-                            <a class="ml-2 text-gray-500">
+                            <a className="ml-2 text-gray-500">
                                 <SiWhatsapp />
                             </a>
                             </span>
                         </div>
-                        <p class="leading-relaxed">{product.descripcion}</p>
-                        <div class="flex items-center pb-5 border-b-2 border-gray-200 mb-5">
+                        <p className="leading-relaxed">{product.descripcion}</p>
+                        <div className="flex items-center pb-5 border-b-2 border-gray-200 mb-5">
 
 
                         </div>
-                        <div class="flex">
-                            <span class="title-font font-medium text-2xl text-gray-900">${product.precio}</span>
+                        <div className="flex">
+                            <span className="title-font font-medium text-2xl text-gray-900">${product.precio}</span>
                             <div className="flex items-center justify-center pl-3">
                                     <div className="flex flex-row border h-10 w-24 rounded-lg border-gray-400 relative">
                                     
@@ -89,10 +80,10 @@ const ItemDetailContainer = () => {
                                     
                                 
                                     </div>
-                                    <h2 class="text-sm title-font text-gray-500 tracking-widest pl-1">Stock disponible: {product.stock}</h2>
+                                    <h2 className="text-sm title-font text-gray-500 tracking-widest pl-1">Stock disponible: {product.stock}</h2>
                             </div>
                         </div>
-                        <div class="pt-4">
+                        <div className="pt-4">
                             <div className="flex items-center justify-center pl-3">
                                     <button onClick={ addCarrito } className="bg-blue-300 hover:bg-blue-500 text-grey-darkest font-bold py-2 px-4 ml-2 rounded">
                                         <span>Agregar</span>
--- src/components/varios/Loading.jsx
+++ src/components/varios/Loading.jsx
@@ -4,15 +4,15 @@ import { ImSpinner9 } from "react-icons/im";
 const Loading = () => {
     return (
         <>
-                <div class="fixed top-0 left-0 z-50 w-screen h-screen flex items-center justify-center">
-                    <div class="bg-white border py-2 px-5 rounded-lg flex items-center flex-col">
-                        <div class="loader-dots block relative w-20 h-5 mt-2">
-                            <div class="absolute top-0 mt-1 w-3 h-3 rounded-full bg-green-500"></div>
-                            <div class="absolute top-0 mt-1 w-3 h-3 rounded-full bg-green-500"></div>
-                            <div class="absolute top-0 mt-1 w-3 h-3 rounded-full bg-green-500"></div>
-                            <div class="absolute top-0 mt-1 w-3 h-3 rounded-full bg-green-500"></div>
+                <div className="fixed top-0 left-0 z-50 w-screen h-screen flex items-center justify-center">
+                    <div className="bg-white border py-2 px-5 rounded-lg flex items-center flex-col">
+                        <div className="loader-dots block relative w-20 h-5 mt-2">
+                            <div className="absolute top-0 mt-1 w-3 h-3 rounded-full bg-green-500"></div>
+                            <div className="absolute top-0 mt-1 w-3 h-3 rounded-full bg-green-500"></div>
+                            <div className="absolute top-0 mt-1 w-3 h-3 rounded-full bg-green-500"></div>
+                            <div className="absolute top-0 mt-1 w-3 h-3 rounded-full bg-green-500"></div>
                         </div>
-                        <div class="text-gray-500 text-xs font-light mt-2 text-center">
+                        <div className="text-gray-500 text-xs font-light mt-2 text-center">
                             Por favor espere...
                         </div>
                     </div>
